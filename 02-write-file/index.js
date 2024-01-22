fc47e0947cc0c66ae5f50dda4c7d17e0df9bdf7b
Igor Bespalov <igorbespalov89@gmail.com>
2024-01-22 19:41:35 +0300

    feat: finish task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+let path = require('path');
+let fs = require('fs');
+let writeStream = fs.createWriteStream(
+  path.join(__dirname, '02-write-file.txt'),
+);
+let { stdout, stdin, exit } = require('process');
+stdout.write('Введите текст:');
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    endFunc();
+  }
+  writeStream.write(data);
+});
+process.on('SIGINT', endFunc);
+function endFunc() {
+  stdout.write('До свидания!');
+  exit();
+}
